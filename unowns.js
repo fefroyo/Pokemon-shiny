5fbb3c3c3caf67ec504dee2f01479853f86439f7
Efrain <5731936+fefroyo@users.noreply.github.com>
2019-03-22 17:16:13 +0100

    unown files
    unown

---
+++ unowns.js
@@ -0,0 +1,366 @@
+let pms = [
+  {
+    "dex": 1,
+    "name": {
+      "de": "Unown A",
+      "en": "Unown A",
+      "fr": "Unown A",
+      "jp": "フシギダネ",
+      "kr": "이상해씨",
+      "zh": "妙蛙種子"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": 2,
+    "name": {
+      "de": "Unown B",
+      "en": "Unown B",
+      "fr": "Unown B",
+      "jp": "フシギソウ",
+      "kr": "이상해풀",
+      "zh": "妙蛙草"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": 3,
+    "name": {
+      "de": "Unown C",
+      "en": "Unown C",
+      "fr": "Unown C",
+      "jp": "フシギバナ",
+      "kr": "이상해꽃",
+      "zh": "妙蛙花"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": 4,
+    "name": {
+      "de": "Unown D",
+      "en": "Unown D",
+      "fr": "Unown D",
+      "jp": "ヒトカゲ",
+      "kr": "파이리",
+      "zh": "小火龍"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": 5,
+    "name": {
+      "de": "Unown E",
+      "en": "Unown E",
+      "fr": "Unown E",
+      "jp": "リザード",
+      "kr": "리자드",
+      "zh": "火恐龍"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": 6,
+    "name": {
+      "de": "Unown F",
+      "en": "Unown F",
+      "fr": "Unown F",
+      "jp": "リザードン",
+      "kr": "리자몽",
+      "zh": "噴火龍"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": 7,
+    "name": {
+      "de": "Unown G",
+      "en": "Unown G",
+      "fr": "Unown G",
+      "jp": "ゼニガメ",
+      "kr": "꼬부기",
+      "zh": "傑尼龜"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": 8,
+    "name": {
+      "de": "Unown H",
+      "en": "Unown H",
+      "fr": "Unown H",
+      "jp": "カメール",
+      "kr": "어니부기",
+      "zh": "卡咪龜"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": 9,
+    "name": {
+      "de": "Unown I",
+      "en": "Unown I",
+      "fr": "Unown I",
+      "jp": "カメックス",
+      "kr": "거북왕",
+      "zh": "水箭龜"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": 10,
+    "name": {
+      "de": "Unown J",
+      "en": "Unown J",
+      "fr": "Unown J",
+      "jp": "キャタピー",
+      "kr": "캐터피",
+      "zh": "綠毛蟲"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": 11,
+    "name": {
+      "de": "Unown K",
+      "en": "Unown K",
+      "fr": "Unown K",
+      "jp": "トランセル",
+      "kr": "단데기",
+      "zh": "鐵甲蛹"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": 12,
+    "name": {
+      "de": "Unown L",
+      "en": "Unown L",
+      "fr": "Unown L",
+      "jp": "バタフリー",
+      "kr": "버터플",
+      "zh": "巴大蝶"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": 13,
+    "name": {
+      "de": "Unown M",
+      "en": "Unown M",
+      "fr": "Unown M",
+      "jp": "ビードル",
+      "kr": "뿔충이",
+      "zh": "獨角蟲"
+    },
+    "shiny_released": false,
+    "family": "Unown"
+  },
+  {
+    "dex": 14,
+    "name": {
+      "de": "Unown N",
+      "en": "Unown N",
+      "fr": "Unown N",
+      "jp": "コクーン",
+      "kr": "딱충이",
+      "zh": "鐵殼蛹"
+    },
+    "shiny_released": false,
+    "family": "Unown"
+  },
+  {
+    "dex": 15,
+    "name": {
+      "de": "Unown O",
+      "en": "Unown O",
+      "fr": "Unown O",
+      "jp": "スピアー",
+      "kr": "독침붕",
+      "zh": "大針蜂"
+    },
+    "shiny_released": false,
+    "family": "Unown"
+  },
+  {
+    "dex": 16,
+    "name": {
+      "de": "Unown P",
+      "en": "Unown P",
+      "fr": "Unown P",
+      "jp": "ポッポ",
+      "kr": "구구",
+      "zh": "波波"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": 17,
+    "name": {
+      "de": "Unown Q",
+      "en": "Unown Q",
+      "fr": "Unown Q",
+      "jp": "ピジョン",
+      "kr": "피죤",
+      "zh": "比比鳥"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": 18,
+    "name": {
+      "de": "Unown R",
+      "en": "Unown R",
+      "fr": "Unown R",
+      "jp": "ピジョット",
+      "kr": "피죤투",
+      "zh": "大比鳥"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": 19,
+    "name": {
+      "de": "Unown S",
+      "en": "Unown S",
+      "fr": "Unown S",
+      "jp": "コラッタ",
+      "kr": "꼬렛",
+      "zh": "小拉達"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": 20,
+    "name": {
+      "de": "Unown T",
+      "en": "Unown T",
+      "fr": "Unown T",
+      "jp": "ラッタ",
+      "kr": "레트라",
+      "zh": "拉達"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": 21,
+    "name": {
+      "de": "Unown U",
+      "en": "Unown U",
+      "fr": "Unown U",
+      "jp": "オニスズメ",
+      "kr": "깨비참",
+      "zh": "烈雀"
+    },
+    "shiny_released": false,
+    "family": "Unown"
+  },
+  {
+    "dex": 22,
+    "name": {
+      "de": "Unown V",
+      "en": "Unown V",
+      "fr": "Unown V",
+      "jp": "オニドリル",
+      "kr": "깨비드릴조",
+      "zh": "大嘴雀"
+    },
+    "shiny_released": false,
+    "family": "Unown"
+  },
+  {
+    "dex": 23,
+    "name": {
+      "de": "Unown W",
+      "en": "Unown W",
+      "fr": "Unown W",
+      "jp": "アーボ",
+      "kr": "아보",
+      "zh": "阿柏蛇"
+    },
+    "shiny_released": false,
+    "family": "Unown"
+  },
+  {
+    "dex": 24,
+    "name": {
+      "de": "Unown X",
+      "en": "Unown X",
+      "fr": "Unown X",
+      "jp": "アーボック",
+      "kr": "아보크",
+      "zh": "阿柏怪"
+    },
+    "shiny_released": false,
+    "family": "Unown"
+  },
+  {
+    "dex": 25,
+    "name": {
+      "de": "Unown Y",
+      "en": "Unown Y",
+      "fr": "Unown Y",
+      "jp": "ピカチュウ",
+      "kr": "피카츄",
+      "zh": "皮卡丘"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": 26,
+    "name": {
+      "de": "Unown Z",
+      "en": "Unown Z",
+      "fr": "Unown Z",
+      "jp": "ライチュウ",
+      "kr": "라이츄",
+      "zh": "雷丘"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": 27,
+    "name": {
+      "de": "Unown ?",
+      "en": "Unown ?",
+      "fr": "Unown ?",
+      "jp": "ライチュウ",
+      "kr": "라이츄",
+      "zh": "雷丘"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": 28,
+    "name": {
+      "de": "Unown !",
+      "en": "Unown !",
+      "fr": "Unown !",
+      "jp": "ライチュウ",
+      "kr": "라이츄",
+      "zh": "雷丘"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+];
