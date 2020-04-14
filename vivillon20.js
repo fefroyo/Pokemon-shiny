73f0ae16f4f34c041df31523543bac8b5fb55013
Efrain <5731936+fefroyo@users.noreply.github.com>
2020-04-14 17:20:43 +0200

    vivillon check

---
+++ vivillon20.js
@@ -0,0 +1,262 @@
+let pms = [
+  {
+    "dex": '666_001',
+    "name": {
+      "de": "Motivo",
+      "en": "Motivo taiga azul",
+      "fr": "Unown A",
+      "jp": "フシギダネ",
+      "kr": "이상해씨",
+      "zh": "妙蛙種子"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": '666_002',
+    "name": {
+      "de": "Motivo",
+      "en": "Motivo tundra azul",
+      "fr": "Unown B",
+      "jp": "フシギソウ",
+      "kr": "이상해풀",
+      "zh": "妙蛙草"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": '666_003',
+    "name": {
+      "de": "Motivo",
+      "en": "Motivo continental amarillo",
+      "fr": "Unown C",
+      "jp": "フシギバナ",
+      "kr": "이상해꽃",
+      "zh": "妙蛙花"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": '666_004',
+    "name": {
+      "de": "Motivo",
+      "en": "Motivo vergel verde",
+      "fr": "Unown D",
+      "jp": "ヒトカゲ",
+      "kr": "파이리",
+      "zh": "小火龍"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": '666_005',
+    "name": {
+      "de": "Motivo",
+      "en": "Motivo oriental morado",
+      "fr": "Unown E",
+      "jp": "リザード",
+      "kr": "리자드",
+      "zh": "火恐龍"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": '666_006',
+    "name": {
+      "de": "Motivo",
+      "en": "Motivo floral rosa",
+      "fr": "Unown F",
+      "jp": "リザードン",
+      "kr": "리자몽",
+      "zh": "噴火龍"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": '666_007',
+    "name": {
+      "de": "Motivo",
+      "en": "Motivo moderno rojo",
+      "fr": "Unown G",
+      "jp": "ゼニガメ",
+      "kr": "꼬부기",
+      "zh": "傑尼龜"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": '666_008',
+    "name": {
+      "de": "Motivo",
+      "en": "Motivo marino azul",
+      "fr": "Unown H",
+      "jp": "カメール",
+      "kr": "어니부기",
+      "zh": "卡咪龜"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": '666_009',
+    "name": {
+      "de": "Motivo",
+      "en": "Motivo isleño marron",
+      "fr": "Unown I",
+      "jp": "カメックス",
+      "kr": "거북왕",
+      "zh": "水箭龜"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": '666_010',
+    "name": {
+      "de": "Motivo",
+      "en": "Motivo estepa marron",
+      "fr": "Unown J",
+      "jp": "キャタピー",
+      "kr": "캐터피",
+      "zh": "綠毛蟲"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": '666_011',
+    "name": {
+      "de": "Motivo",
+      "en": "Motivo desierto marron",
+      "fr": "Unown K",
+      "jp": "トランセル",
+      "kr": "단데기",
+      "zh": "鐵甲蛹"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": '666_012',
+    "name": {
+      "de": "Motivo",
+      "en": "Motivo oasis marron",
+      "fr": "Unown L",
+      "jp": "バタフリー",
+      "kr": "버터플",
+      "zh": "巴大蝶"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": '666_013',
+    "name": {
+      "de": "Motivo",
+      "en": "Motivo monzón gris",
+      "fr": "Unown M",
+      "jp": "ビードル",
+      "kr": "뿔충이",
+      "zh": "獨角蟲"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": '666_014',
+    "name": {
+      "de": "Motivo",
+      "en": "Motivo pantano verde",
+      "fr": "Unown N",
+      "jp": "コクーン",
+      "kr": "딱충이",
+      "zh": "鐵殼蛹"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": '666_015',
+    "name": {
+      "de": "Motivo",
+      "en": "Motivo solar rojo",
+      "fr": "Unown O",
+      "jp": "スピアー",
+      "kr": "독침붕",
+      "zh": "大針蜂"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": '666_016',
+    "name": {
+      "de": "Motivo",
+      "en": "Motivo océano rojo",
+      "fr": "Unown P",
+      "jp": "ポッポ",
+      "kr": "구구",
+      "zh": "波波"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": '666_017',
+    "name": {
+      "de": "Motivo",
+      "en": "Motivo jungla verde",
+      "fr": "Unown Q",
+      "jp": "ピジョン",
+      "kr": "피죤",
+      "zh": "比比鳥"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": '666_018',
+    "name": {
+      "de": "Motivo",
+      "en": "Motivo fantasía rosa",
+      "fr": "Unown R",
+      "jp": "ピジョット",
+      "kr": "피죤투",
+      "zh": "大比鳥"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": '666_019',
+    "name": {
+      "de": "Motivo",
+      "en": "Motivo poké ball rojo",
+      "fr": "Unown S",
+      "jp": "コラッタ",
+      "kr": "꼬렛",
+      "zh": "小拉達"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+  {
+    "dex": '666_000',
+    "name": {
+      "de": "Motivo",
+      "en": "Motivo polar blanco",
+      "fr": "Unown T",
+      "jp": "ラッタ",
+      "kr": "레트라",
+      "zh": "拉達"
+    },
+    "shiny_released": true,
+    "family": "Unown"
+  },
+];
